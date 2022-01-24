63f9b8901d4241e1a7bdb3aad402c825f2c250cd
Daniel Grahn <dangrahn@gmail.com>
2022-01-24 23:37:06 +0100

    chore: java 15

--- java/install.sh
+++ java/install.sh
@@ -2,5 +2,6 @@
 
 # Install Java
 brew tap adoptopenjdk/openjdk
-brew install --cask adoptopenjdk14
+#brew install --cask adoptopenjdk14
 brew install --cask adoptopenjdk15
+#brew install --cask adoptopenjdk16
