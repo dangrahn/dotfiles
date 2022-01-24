7f26ec4f6031646e4226d65dc1a9e618dfcf930c
Daniel Grahn <dangrahn@gmail.com>
2022-01-24 23:36:24 +0100

    fix: android path

--- android/path.zsh
+++ android/path.zsh
@@ -1,5 +1,6 @@
 export ANDROID_HOME=/Users/$USER/Library/Android/sdk
 
 export PATH=$PATH:$ANDROID_HOME/tools
+export PATH=$PATH:$ANDROID_HOME/tools/bin
 export PATH=$PATH:$ANDROID_HOME/platform-tools
 export PATH=$PATH:$ANDROID_HOME/emulator
